338d1cca353e8768354609cace0340f9cef63298
dimaakimm <dmitriykim2005@gmail.com>
2026-05-11 10:07:20 +0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,69 +1,70 @@
 import path from "node:path";
 
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin(
-  "./src/entities/locale/lib/request.ts",
-);
+const nextIntlConfigPath = "./src/entities/locale/lib/request.ts";
+const nextIntlConfigAlias = path.resolve(nextIntlConfigPath);
 
 const nextConfig = {
-  reactStrictMode: false,
-  output: "standalone",
-  outputFileTracingRoot: path.resolve("."),
-  compiler: {
-    styledComponents: true,
-  },
-  productionBrowserSourceMaps: false,
-  eslint: { ignoreDuringBuilds: true },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias["prom-client"] = false;
-    }
-    return config;
-  },
-  experimental: {
-    optimizePackageImports: [
-      "antd",
-      "react-virtuoso",
-      "@mdxeditor/editor",
-      "@virtuoso.dev/message-list",
-      "dayjs",
-      "lodash.debounce",
+    reactStrictMode: false,
+    output: "standalone",
+    outputFileTracingRoot: path.resolve("."),
+    compiler: {
+        styledComponents: true,
+    },
+    productionBrowserSourceMaps: false,
+    webpack: (config, { isServer }) => {
+        config.resolve.alias["next-intl/config"] = nextIntlConfigAlias;
+
+        if (!isServer) {
+            config.resolve.alias["prom-client"] = false;
+        }
+        return config;
+    },
+    turbopack: {
+        resolveAlias: {
+            "next-intl/config": nextIntlConfigPath,
+        },
+    },
+    experimental: {
+        optimizePackageImports: [
+            "antd",
+            "react-virtuoso",
+            "@mdxeditor/editor",
+            "@virtuoso.dev/message-list",
+            "dayjs",
+            "lodash.debounce",
+        ],
+    },
+    transpilePackages: [
+        "three",
+        "rc-util",
+        "@ant-design",
+        "kitchen-flow-editor",
+        "@ant-design/pro-editor",
+        "zustand",
+        "leva",
+        "antd",
+        "rc-pagination",
+        "rc-picker",
     ],
-  },
-  transpilePackages: [
-    "three",
-    "rc-util",
-    "@ant-design",
-    "kitchen-flow-editor",
-    "@ant-design/pro-editor",
-    "zustand",
-    "leva",
-    "antd",
-    "rc-pagination",
-    "rc-picker",
-  ],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-        port: "",
-        pathname: "**",
-      },
-    ],
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home-page",
-        permanent: true,
-      },
-    ];
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+                port: "",
+                pathname: "**",
+            },
+        ],
+    },
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/home-page",
+                permanent: true,
+            },
+        ];
+    },
 };
 
-const configWithIntl = withNextIntl(nextConfig);
-
-export default configWithIntl;
+export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,41 @@
 import { PropsWithChildren } from "react";
-import { Sansation } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, setRequestLocale } from "next-intl/server";
 
 import { LanguageType } from "@/entities/locale";
 import { WithProviders } from "@/app/(providers)";
 import {
-  createLocalizedPageMetadata,
-  getWebsiteJsonLd,
-  toJsonLd,
+    createLocalizedPageMetadata,
+    getWebsiteJsonLd,
+    toJsonLd,
 } from "@/shared/config/seo";
 import "./(theme)/global.css";
 
-const sansation = Sansation({
-  weight: ["300", "400", "700"],
-  subsets: ["latin", "cyrillic"],
-  variable: "--font-sansation",
-});
-
 export const generateMetadata = () =>
-  createLocalizedPageMetadata("seo.pages.homePage", {
-    path: "/home-page",
-    index: true,
-    follow: true,
-  });
+    createLocalizedPageMetadata("seo.pages.homePage", {
+        path: "/home-page",
+        index: true,
+        follow: true,
+    });
 
 export default async function RootLayout({ children }: PropsWithChildren) {
-  const locale: LanguageType = "ru";
-  setRequestLocale(locale);
+    const locale: LanguageType = "ru";
+    setRequestLocale(locale);
 
-  const messages = await getMessages();
-  const websiteJsonLd = getWebsiteJsonLd();
+    const messages = await getMessages();
+    const websiteJsonLd = getWebsiteJsonLd();
 
-  return (
-    <html lang={locale}>
-      <body className={`${sansation.variable} font-sans`}>
+    return (
+        <html lang={locale}>
+        <body>
         <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: toJsonLd(websiteJsonLd) }}
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: toJsonLd(websiteJsonLd) }}
         />
         <NextIntlClientProvider messages={messages} locale={locale}>
-          <WithProviders>{children}</WithProviders>
+            <WithProviders>{children}</WithProviders>
         </NextIntlClientProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
